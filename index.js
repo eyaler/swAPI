c4744104c4397fbb772b6eff58f661c4c3d45388
Eyal Gruss <eyalgruss@gmail.com>
2025-09-11 00:20:45 +0300

    add inserts

--- index.js
+++ index.js
@@ -6,11 +6,12 @@ const boxHeightJumpFactor = 1.3
 const indexDeltaY = 35
 const animationRate = 0.01
 const delayRate = 0.01
+const realignWeight = 0.99
 let boxHeight
 let boxes
-let draggingBox
-let targetBox
-let tempBox
+let firstBox
+let secondBox
+let thirdBox
 let animationProgress
 let animationFinish
 let isAnimating
@@ -26,33 +27,43 @@ function drawBoxes() {
 	if (!values)
 		return
 	background(240)
+	textAlign(CENTER, CENTER)
 	
 	if (!isAnimating && queue.length) {
-		const [i, j, kOrSwap] = queue.shift()
+		const [i, j, kOrMode] = queue.shift()
 		isAnimating = true
 		animationProgress = -1
 		animationFinish = 0
-		tempBox = null
-		draggingBox = boxes[i]
-		targetBox = boxes[j]
-		if (kOrSwap === true) {
+		firstBox = boxes[i]
+		secondBox = boxes[j]
+		thirdBox = null
+		if (kOrMode === true || typeof kOrMode == 'string') {
 			animationFinish = 1
-			const saveX = targetBox.lastX
-			const saveY = targetBox.lastY
-			targetBox.lastX = draggingBox.lastX
-			targetBox.lastY = draggingBox.lastY
-			draggingBox.lastX = saveX
-			draggingBox.lastY = saveY
-			;[boxes[i], boxes[j]] = [boxes[j], boxes[i]]
-			;[values[i], values[j]] = [values[j], values[i]]
-		} else if (kOrSwap !== false)
-			tempBox = boxes[kOrSwap]
+			const saveX = secondBox.lastX
+			const saveY = secondBox.lastY
+			secondBox.lastX = firstBox.lastX
+			secondBox.lastY = firstBox.lastY
+			firstBox.lastX = saveX
+			firstBox.lastY = saveY	
+			if (kOrMode === true || kOrMode.toLowerCase() == 'swap') {
+				;[boxes[i], boxes[j]] = [boxes[j], boxes[i]]
+				;[values[i], values[j]] = [values[j], values[i]]
+			} else if (i != j) {
+				boxes.splice(j, 1)
+				const newI = boxes.indexOf(firstBox) + (kOrMode.toLowerCase() == 'after')
+				boxes.splice(newI, 0, secondBox)
+				values.splice(newI, 0, values.splice(j, 1)[0])
+				firstBox.passive = true
+			}
+		} else if (kOrMode !== false)
+			thirdBox = boxes[kOrMode]
 	}
 	
 	textSize(20)
 	fill('black')
+	const w = boxWidth()
 	for (let i = 0; i < boxes.length; i++)
-		text(i, boxWidth() * (marginFrac+0.5+i*(1+marginFrac)) * width, (rowHeight+boxHeight/2)*height + indexDeltaY)
+		text(i, w * (marginFrac+0.5+i*(1+marginFrac)) * width, (rowHeight+boxHeight/2)*height + indexDeltaY)
 	for (let i = 0; i < boxes.length; i++)
 		drawBox(boxes[i])
 
@@ -65,47 +76,58 @@ function drawBoxes() {
 			isAnimating = false
 		}
 
-		const centerX = (targetBox.lastX+draggingBox.lastX) / 2
-		const centerY = (targetBox.lastY+draggingBox.lastY) / 2
+		const centerX = (secondBox.lastX+firstBox.lastX) / 2
+		const centerY = (secondBox.lastY+firstBox.lastY) / 2
 		const angle = PI * animationProgress
-		draggingBox.x = centerX + cos(angle)*(targetBox.lastX-centerX)
-		draggingBox.y = sin(angle)*boxHeight*boxHeightJumpFactor*(centerY <= rowHeight ? -1 : 1) + targetBox.lastY + animationProgress*(targetBox.lastY-draggingBox.lastY)
-		targetBox.x = centerX + cos(angle)*(draggingBox.lastX-centerX)
-		targetBox.y = sin(-angle)*boxHeight*boxHeightJumpFactor*(centerY <= rowHeight ? -1 : 1) + draggingBox.lastY + animationProgress*(targetBox.lastY-draggingBox.lastY)
+		if (!firstBox.passive) {
+			firstBox.x = centerX + cos(angle)*(secondBox.lastX-centerX)
+			firstBox.y = sin(-angle)*boxHeight*boxHeightJumpFactor*(centerY <= rowHeight ? -1 : 1) + secondBox.lastY + animationProgress*(secondBox.lastY-firstBox.lastY)
+		}
+		secondBox.x = centerX + cos(angle)*(firstBox.lastX-centerX)
+		secondBox.y = sin(angle)*boxHeight*boxHeightJumpFactor*(centerY <= rowHeight ? -1 : 1) + firstBox.lastY + animationProgress*(secondBox.lastY-firstBox.lastY)
+		for (let i = 0; i < values.length; i++)
+			if ((boxes[i] != firstBox || firstBox.passive) && boxes[i] != secondBox)
+				boxes[i].x = boxes[i].x + (w*(marginFrac+i*(1+marginFrac))-boxes[i].x)*(1-0.01**animationRate)
 		if (!isAnimating) {
-			targetBox = null
-			draggingBox = null
-			tempBox = null
+			firstBox.passive = false
+			secondBox = null
+			firstBox = null
+			thirdBox = null
+			for (let i = 0; i < values.length; i++)
+				boxes[i].lastX = boxes[i].x
 		}
 	}
 }
 
 function drawBox(box) {
+	const xw = box.x * width
+	const yh = box.y * height
+	const ww = boxWidth() * width
+	const hh = boxHeight * height
+	
 	noStroke()
 	fill(0, 50)
-	rect(box.x*width + 5, box.y*height + 5, boxWidth() * width, boxHeight * height, 10)
+	rect(xw + 5, yh + 5, ww, hh, 10)
 	
-	if (box == draggingBox)
+	if (box == firstBox)
 	  fill(150, 250, 100)
-	else if (box == targetBox)
+	else if (box == secondBox)
 	  fill(250, 100, 150)
-	else if (box == tempBox)
+	else if (box == thirdBox)
 		fill(250, 100, 250)
 	else
 	  fill(100, 150, 250)
 	stroke('black')
 	strokeWeight(2)
-	rect(box.x * width, box.y * height, boxWidth() * width, boxHeight * height, 10)
+	rect(xw, yh, ww, hh, 10)
 
 	noStroke()
 	fill('white')
-	textSize(min(30, boxWidth() * width * 0.8 * 2 / maxDigits))
-	textAlign(CENTER, CENTER)
-	text(box.value, (box.x+boxWidth()/2) * width, (box.y+boxHeight/2) * height)
+	textSize(min(30, ww * 0.8 * 2 / maxDigits))
+	text(box.value, xw + ww/2, yh + hh/2)
 }
 
 function setupBoxes(numbers) {
-	textAlign(CENTER, CENTER)
 	if (numbers == null)
 		throw Error('You forgot to pass an array to setupBoxes()')
 	values = [...numbers]
@@ -120,8 +142,8 @@ function setupBoxes(numbers) {
 	}
 }
 
-function animateBoxes(i, j, kOrSwap=true) {
+function animateBoxes(i, j, kOrMode=true) {
 	if (i == null || j == null)
-		throw Error('You forgot to pass two indices to animateBoxes()')
-	queue.push([i, j, kOrSwap])
+		throw Error('You forgot to pass at least two indices to animateBoxes()')
+	queue.push([i, j, kOrMode])
 }
